1ea2ceb99342ce5251d2ef57ee8d52e3f1099062
Markus Schirp <mbj@schirp-dso.com>
2013-06-29 17:07:46 +0200

    Use spec setup already done by devtools

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,7 @@
 # encoding: utf-8
 
-require 'devtools/spec_helper'
-
-if ENV['COVERAGE'] == 'true'
-  require 'simplecov'
-  require 'coveralls'
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-
-  SimpleCov.start do
-    command_name     'spec:unit'
-    add_filter       'config'
-    add_filter       'spec'
-    minimum_coverage 100
-  end
-end
-
 require 'abstract_type'
-
-# require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
-  require file
-end
+require 'devtools/spec_helper'
 
 RSpec.configure do |config|
 end
